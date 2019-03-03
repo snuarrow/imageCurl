84568ac8518da530037db39bab06b6460638aad1
henri.vaara <henri.vaara@cgi.com>
2019-03-03 17:10:49 +0200

    run.sh updated

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 docker-compose -f db/docker-compose.yml up -d
+go get github.com/gin-gonic/gin
+go get github.com/lib/pq
+go get github.com/rwcarlsen/goexif/exif
+go get github.com/rwcarlsen/goexif/mknote
+go get github.com/umahmood/haversine
 go run go/src/main.go
